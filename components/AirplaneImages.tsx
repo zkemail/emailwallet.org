6ab0a9b548a07228af7bd69da19763440125358a
George <xsova113@gmail.com>
2023-09-18 00:31:49 -0700

    add more airplane animation

--- components/AirplaneImages.tsx
+++ components/AirplaneImages.tsx
@@ -2,48 +2,48 @@ import Image from "next/image";
 
 const AirplaneImages = () => {
   return (
-    <div className="group right-0 h-[200%] w-1/2 overflow-x-clip max-md:hidden">
+    <div className="group ml-20 h-[130%] w-1/2 overflow-x-clip max-md:hidden">
       <Image
         src={"/paperplane.png"}
         alt={"paper plane image"}
         width={150}
         height={150}
-        className="absolute right-1/3 top-1/4"
+        className="absolute right-1/3 top-1/4 rotate-45 transition duration-700 group-hover:translate-x-[400px] group-hover:translate-y-[100px]"
       />
       <Image
         src={"/paperplane1.png"}
         alt={"paper plane image"}
         width={150}
         height={150}
-        className="absolute -right-10 top-1/3"
+        className="absolute -right-10 top-1/3 -rotate-45 transition duration-700 group-hover:-translate-x-[700px] group-hover:translate-y-[400px]"
       />
       <Image
         src={"/paperplane2.png"}
         alt={"paper plane image"}
         width={150}
         height={150}
-        className="absolute right-1/3 top-2/3 transition  duration-500 group-hover:-translate-y-[400px]"
+        className="absolute right-1/3 top-2/3 transition duration-700 group-hover:-translate-y-[400px]"
       />
       <Image
         src={"/paperplane3.png"}
         alt={"paper plane image"}
         width={150}
         height={150}
-        className="absolute -right-5 bottom-1/4"
+        className="absolute -right-5 bottom-1/4 transition duration-700 group-hover:-translate-x-[700px] group-hover:-translate-y-24"
       />
       <Image
         src={"/paperplane4.png"}
         alt={"paper plane image"}
         width={150}
         height={150}
-        className="absolute bottom-[37%] right-1/4"
+        className="absolute bottom-[37%] right-1/4 transition duration-700 group-hover:-translate-x-[400px] group-hover:-translate-y-[400px]"
       />
       <Image
         src={"/paperplane6.png"}
         alt={"paper plane image"}
         width={150}
         height={150}
-        className="absolute right-24 top-1/4"
+        className="absolute right-24 top-1/4 transition duration-700 group-hover:-translate-x-[600px] group-hover:translate-y-64"
       />
     </div>
   );
