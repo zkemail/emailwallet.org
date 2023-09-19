8123702112633e7998b4338326e849caf6dcc339
George <xsova113@gmail.com>
2023-09-18 19:56:40 -0700

    modify nextconfig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: { unoptimized: true },
-  trailingSlash: true,
-  output: "export",
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
