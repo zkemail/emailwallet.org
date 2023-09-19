1c1c63344a759472359144efc2596fb9d07f0540
George <xsova113@gmail.com>
2023-09-18 20:56:27 -0700

    big fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { trailingSlash: true, output: "export" };
+const nextConfig = {
+  trailingSlash: true,
+  output: "export",
+  images: { unoptimized: true },
+};
 
 module.exports = nextConfig;
