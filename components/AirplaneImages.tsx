0732e79792645bef7df7503b7f746b8aacb43cfd
George <xsova113@gmail.com>
2023-09-18 20:54:07 -0700

    bug fix

--- components/AirplaneImages.tsx
+++ components/AirplaneImages.tsx
@@ -1,42 +1,44 @@
+import Image from "next/image";
+
 const AirplaneImages = () => {
   return (
     <div className="group ml-20 h-[130%] w-1/2 overflow-x-clip max-md:hidden">
-      <img
+      <Image
         src={"/paperplane.png"}
         alt={"paper plane image"}
         width={150}
         height={150}
         className="absolute right-1/3 top-1/4 rotate-45 transition duration-700 group-hover:translate-x-[400px] group-hover:translate-y-[100px]"
       />
-      <img
+      <Image
         src={"/paperplane1.png"}
         alt={"paper plane image"}
         width={150}
         height={150}
         className="absolute -right-10 top-1/3 -rotate-45 transition duration-700 group-hover:-translate-x-[700px] group-hover:translate-y-[400px]"
       />
-      <img
+      <Image
         src={"/paperplane2.png"}
         alt={"paper plane image"}
         width={150}
         height={150}
         className="absolute right-1/3 top-2/3 transition duration-700 group-hover:-translate-y-[400px]"
       />
-      <img
+      <Image
         src={"/paperplane3.png"}
         alt={"paper plane image"}
         width={150}
         height={150}
         className="absolute -right-5 bottom-1/4 transition duration-700 group-hover:-translate-x-[700px] group-hover:-translate-y-24"
       />
-      <img
+      <Image
         src={"/paperplane4.png"}
         alt={"paper plane image"}
         width={150}
         height={150}
         className="absolute bottom-[37%] right-1/4 transition duration-700 group-hover:-translate-x-[400px] group-hover:-translate-y-[400px]"
       />
-      <img
+      <Image
         src={"/paperplane6.png"}
         alt={"paper plane image"}
         width={150}
