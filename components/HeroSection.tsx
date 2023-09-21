657991155412585d03fd35ab670ad2fa28a0e4c5
George <xsova113@gmail.com>
2023-09-20 23:47:59 -0700

    update hero section animation

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -24,24 +24,42 @@ const HeroSection = () => {
           viewport={{ once: false, amount: 0.25 }}
           className="flex flex-col items-start gap-y-6 xl:gap-y-10"
         >
-          <h1 className="text-4xl font-semibold leading-tight md:text-6xl md:leading-tight">
+          <h1 className="text-4xl font-semibold leading-tight md:text-5xl md:leading-tight lg:text-6xl">
             Email money to{" "}
             <TypingText
               className="w-fit rounded-xl bg-tertiary px-2"
               title={"anyone."}
             />
           </h1>
-          <h3 className="text-lg text-muted-foreground dark:text-slate-200">
+          <motion.h3
+            initial="hidden"
+            whileInView="visible"
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            className="text-lg text-muted-foreground dark:text-slate-200"
+          >
             We use zk proofs to ensure you own your money.
-          </h3>
-          <div className="flex gap-4">
+          </motion.h3>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+            className="flex gap-4"
+          >
             <Button className="rounded-lg bg-tertiary px-8 text-primary hover:bg-tertiary-foreground">
               Try Demo
             </Button>
             <Button className="rounded-lg border-2 px-8" variant={"outline"}>
               Read Docs
             </Button>
-          </div>
+          </motion.div>
         </motion.div>
 
         {/* Balls and airplanes images */}
