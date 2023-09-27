3b24eedb9d76002cf17ec9b36baa464bc258bf0d
George <xsova113@gmail.com>
2023-09-27 11:45:08 -0700

    update styling

--- components/SendToPeopleSection.tsx
+++ components/SendToPeopleSection.tsx
@@ -1,14 +1,36 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 const SendToPeopleSection = () => {
   return (
     <section className="mx-8 flex max-w-screen-lg items-center max-md:text-end md:mx-20">
       <div className="mb-20 ml-auto flex flex-col gap-6 md:basis-[50%]">
-        <h1 className="text-3xl font-semibold md:text-4xl">
+        <motion.h1
+          initial="hidden"
+          whileInView="visible"
+          variants={{
+            hidden: { opacity: 0, x: 50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          transition={{ duration: 0.5 }}
+          className="text-3xl font-semibold md:text-4xl"
+        >
           Send to people outside the system.
-        </h1>
-        <p className="text-sm text-muted-foreground">
+        </motion.h1>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          variants={{
+            hidden: { opacity: 0, x: 50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="text-sm text-muted-foreground"
+        >
           Send to anyone with an email address, whether or not they have an
           account with us yet.
-        </p>
+        </motion.p>
       </div>
     </section>
   );
--- components/YouOwnMoneySection.tsx
+++ components/YouOwnMoneySection.tsx
@@ -11,13 +11,31 @@ const YouOwnMoneySection = () => {
   return (
     <section className="mx-8 mb-60 mt-32 flex max-w-screen-lg items-center md:mx-20 md:my-60">
       <div className="flex flex-col gap-6">
-        <h1 className="text-3xl font-semibold md:text-4xl">
+        <motion.h1
+          initial="hidden"
+          whileInView="visible"
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          transition={{ duration: 0.5 }}
+          className="text-3xl font-semibold md:text-4xl"
+        >
           You own your money.
-        </h1>
-        <p className="text-sm text-muted-foreground">
+        </motion.h1>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className="text-sm text-muted-foreground"
+        >
           We can&apos;t steal your funds, they can only be authorized by your
           email.
-        </p>
+        </motion.p>
       </div>
       <motion.div
         style={{ x: scrollXProgress }}
