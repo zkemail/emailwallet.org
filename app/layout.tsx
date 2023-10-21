aebb182b375de8cd77c16746722f392b8396570c
George <xsova113@gmail.com>
2023-10-20 21:51:37 -0700

    minor update

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,14 +23,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
-          integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
-          crossOrigin="anonymous"
-        />
-      </Head>
       <body
         className={cn(inter.className, "overflow-x-clip overflow-y-scroll")}
       >
