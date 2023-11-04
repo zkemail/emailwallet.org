c945b8bb6ebe512e11216cffa8c7159fc3868a6f
George <xsova113@gmail.com>
2023-11-04 14:02:42 -0700

    update files structure

--- app/(main)/(routes)/app/_components/Form.tsx
+++ app/(main)/(routes)/app/_components/Form.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { Button, buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { useEffect, useRef, useState } from "react";
 
 enum Currency {
