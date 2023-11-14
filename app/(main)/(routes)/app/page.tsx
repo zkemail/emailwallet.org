b4db7db7793d249788369afce4702c372a404c37
Atris <vacekj@outlook.com>
2023-11-14 13:04:41 +0100

    fix: footer overlap

--- app/(main)/(routes)/app/page.tsx
+++ app/(main)/(routes)/app/page.tsx
@@ -2,10 +2,10 @@ import Form from "./_components/Form";
 
 export default function SendMoneyPage() {
   return (
-    <div className="mb-56 h-full">
+    <div className="mb-10">
       <div className="gap- flex flex-col items-center gap-[5rem] py-[5rem] sm:py-24">
         <h1 className="text-4xl font-medium">Send Money via Email</h1>
-        <h2 className=" text-center text-muted-foreground">
+        <h2 className="text-center text-muted-foreground">
           This sends money from your currently logged-in email address, by
           emailing the relayer with the amount and recipient in your subject!
           <br /> Everyone starts with 100 TEST tokens.
